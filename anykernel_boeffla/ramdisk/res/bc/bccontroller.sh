1d6a2d2ce7ac33c81f5f205f64f15d13884ed581
andip71 <andreasp@gmx.de>
2016-07-28 19:14:23 +0200

    Boeffla ramfs: Kernel URL domain change

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -13,7 +13,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="g900f;cm;cm13.0;http://boeffla.df-kunde.de/sgs5/boeffla-kernel-cm/"
+KERNEL_SPECS="g900f;cm;cm13.0;http://kernel.boeffla.de/sgs5/boeffla-kernel-cm/"
 
 # kernel features 
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
